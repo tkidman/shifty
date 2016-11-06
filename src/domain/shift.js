a84d7033377d29cb89edb28d8f66850433f8a3fd
Tom Kidman <Tom.Kidman@team.telstra.com>
2016-11-06 18:05:31 +1100

    working on filling shifts

--- src/domain/shift.js
+++ src/domain/shift.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const shiftTypes = require('./shiftType');
+
 class Shift {
   constructor(params) {
     this.type = params.type;
@@ -14,6 +16,26 @@ class Shift {
 
   addAvailableEmployee(employee) {
     this.availableEmployees.push(employee);
+  }
+
+  fill() {
+    while (this.allocatedEmployees.length < this.type.numEmployees) {
+      const employee = this.findBestEmployee();
+      employee.allocateToShift(this);
+    }
+  }
+
+  findBestEmployee() {
+    // give each available employee a score to sort on
+    const score = 0; // lowest is best
+    this.availableEmployees.sort((first, second) => {
+
+    });
+    return this.availableEmployees.first();
+  }
+
+  calculateEmployeeShiftSuitablityScore(employee) {
+    if (this.type != nig)
   }
 }
 
